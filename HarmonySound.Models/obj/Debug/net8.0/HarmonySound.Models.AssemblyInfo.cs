c4aa1404b9aab1f51c6cafa7d7cc09d582250d95
Alexander <thevector1999@gmail.com>
2025-07-02 17:04:21 -0500

    Agregar .gitignore

--- HarmonySound.Models/obj/Debug/net8.0/HarmonySound.Models.AssemblyInfo.cs
+++ HarmonySound.Models/obj/Debug/net8.0/HarmonySound.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HarmonySound.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a79f859a4ba53e24388e699bf9514e266f07f45")]
 [assembly: System.Reflection.AssemblyProductAttribute("HarmonySound.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HarmonySound.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
